c2b1dde8678129ff63b7488295933bdf07f23f8b
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2021-06-26 18:19:06 -0700

    Added UI changes and directions for users

--- ResourcesAndCode/webapp.py
+++ ResourcesAndCode/webapp.py
@@ -1,9 +1,12 @@
+from altair.vegalite.v4.schema.channels import Color
 from numpy.core.fromnumeric import size
 import streamlit as st
 import pandas as pd
 import pickle
 from PIL import Image
-st.title("Cardio Disease Detection")
+
+st.title("Crush CHD")
+st.write("Coronary heart disease (CHD) is an extremly serious disease. It is now one of the most leading causes of death worldwide, even in developed countries. About 3.8 million men and 3.4 million women die every year from this disease. Luckily, Crush CHD can help. In a matter of seconds, without having to go to a doctor, you can test to see whether or not you have Coronary Heart Disease. Not only is this method non-invasive, it is also reliably accurate, having accuracy rates that are higher than standard methods, which hover around the low 70 percent accuracy reates.")
 image = Image.open("C:/Users/assist/Desktop/Machine Learning Projects/AppDevLeaugeHackathonGithubProject/ResourcesAndCode/robina-weermeijer-NIuGLCC7q54-unsplash.jpg")
 st.image(image, width = 290)
 model = pickle.load(open("ResourcesAndCode\HeartDiseaseDetectModel.pickle", 'rb'))
@@ -23,12 +26,39 @@ df = pd.read_csv("ResourcesAndCode\CHDdata.csv")
 df = df.drop("famhist", axis = 1)
 df = df.drop("typea", axis=1)
 
-st.write("Some people before you had these results: ")
-st.dataframe(data = df.head(), width = 500, height = 500)
+st.write("A few patients before you had these results. 0 represents healthy and 1 represents those who have the disease: ")
+st.dataframe(data = df.head(10), width = 500, height = 500)
 
 st.text("")
 st.text("")
-st.sidebar.write("Predict whether or not you have Coronary Heart Disease")
+
+st.write("Below is some data about other patients who correspond to possibly being diagnosed with Coronary Heart Disease. Data charts are scalable in both values and size. Charts can also be downloaded in SVG and PNG format.")
+
+st.write("Overall: ")
+chart = st.bar_chart(df)
+
+st.write("Adiposity rates among patients: ")
+st.bar_chart(df['adiposity'])
+
+st.write("Age among patients: ")
+st.bar_chart(df['age'])
+
+st.write("Alcohol usage among patients. (In liters): ")
+st.bar_chart(df['alcohol'])
+
+st.write("Low Density Lipoprotein (LDL) rates among patients: ")
+st.bar_chart(df['ldl'])
+
+st.write("Obesity/Body Mass Index (BMI) rates: ")
+st.bar_chart(df['obesity'])
+
+st.write("Stystolic Blood Pressure (SBP) among paitents: ")
+st.bar_chart(df['sbp'])
+
+st.write("Tobacco use (In Kilograms) among patients: ")
+st.bar_chart(df['tobacco'])
+
+st.sidebar.write("Directions: Enter in your data. All values are defaulted to zero. After data is inputted, all you need to do is click, 'Predict Results', sit back, and wait a few seconds for your answer!")
 def get_user_input(): 
     sbp = st.sidebar.slider("Stystolic Blood Pressure", min_value=0, max_value=300)
     tobacco = st.sidebar.slider("Yearly Tobacco Usage in Kilograms", min_value = 0, max_value = 75)
@@ -41,7 +71,7 @@ def get_user_input():
 
 users_input = get_user_input()
 
-if (st.sidebar.button("Predict whether or not this person is diseased")): 
+if (st.sidebar.button("Predict Results")): 
     prediction = predictIt(users_input)
     if (prediction[0] == 1): 
         st.sidebar.text("")
