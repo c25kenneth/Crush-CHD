72312f523dec93deb74269b77c756cb9c81e0f6d
thequickbrownfoxjumpedoverthelazydog <c25kenneth@gmail.com>
2021-07-01 22:50:53 -0700

    Added a better description on disease to web app

--- ResourcesAndCode/webapp.py
+++ ResourcesAndCode/webapp.py
@@ -6,7 +6,7 @@ import pickle
 from PIL import Image
 
 st.title("Crush CHD")
-st.write("Coronary heart disease (CHD) is an extremely serious disease. It is now one of the most leading causes of death worldwide, even in developed countries. About 3.8 million men and 3.4 million women die every year from this disease. Luckily, Crush CHD can help. In a matter of seconds, without having to go to a doctor, an AI/Machine Learning model will help determine whether or not you have Coronary Heart Disease. Not only is this method non-invasive, it is also reliably accurate, having accuracy rates that are higher than standard methods. Standard methods hover around the low 70 percent mark, while Crush CHD has an accuracy rate of almost 80%.")
+st.write("Coronary heart disease (CHD) is an extremely serious disease. The disease occurs when the arteries clog up from plaque and cannot deliver enough blood to the heart. It is now one of the most leading causes of death worldwide, even in developed countries. About 3.8 million men and 3.4 million women die every year from this disease. Luckily, Crush CHD can help. In a matter of seconds, without having to go to a doctor, an AI/Machine Learning model will help determine whether or not you have Coronary Heart Disease. Not only is this method non-invasive, it is also reliably accurate, having accuracy rates that are higher than standard methods. Standard methods hover around the low 70 percent mark, while Crush CHD has an accuracy rate of almost 80%.")
 image = Image.open("C:/Users/assist/Desktop/Machine Learning Projects/AppDevLeaugeHackathonGithubProject/ResourcesAndCode/Heart-Picture.jpg")
 st.image(image, width = 290)
 model = pickle.load(open("ResourcesAndCode\HeartDiseaseDetectModel.pickle", 'rb'))
